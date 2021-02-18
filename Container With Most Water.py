f35eb534d422335dd699ee09026de2cff6b0f8a9
Shreyas Gopalakrishna <11889130+shreyas-gopalakrishna@users.noreply.github.com>
2021-02-18 12:10:07 +0530

    Update Container With Most Water.py

--- Container With Most Water.py
+++ Container With Most Water.py
@@ -1,17 +1,18 @@
 # https://leetcode.com/problems/container-with-most-water/
 # Logic: Take 2 pointers, start and end, and move them such that we retain 
 #        the height which is more and keep updating the area
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        i,j = 0,len(height)-1
-        maxi,maxj = i,j
-        maxA = 0
+        i,j = 0, len(height)-1
+        
+        result = 0
+        
         while(i < j):
-            A = min(height[i],height[j]) * (j-i)
-            if(A > maxA):
-                maxA = A
+            result = max(result, (j-i) * min(height[i], height[j]))
+            
             if(height[i] < height[j]):
-                i+=1
+                i += 1
             else:
-                j-=1
-        return maxA
+                j -= 1
+        return result
